a9656023f137d3f73b0d43a0cea31c68ea99c3d7
Brais <braisva@gmail.com>
2018-12-04 23:40:54 +0100

    Initial commit
    webpack4 & sass compiling to css output

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const devMode = process.env.NODE_ENV !== 'production'
+
+const path = require('path');
+module.exports = {
+  entry: {
+      main: './src/index.js',
+      //styles: './src/styles/style.scss',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles.css"
+    })
+  ]
+};
