0efa3ecf4f2508c60734badbd440b020c7a235e6
Brais <braisva@gmail.com>
2019-06-19 13:38:09 +0200

    Mobile

--- docs/runtime.4863bbca4e25c651c861.js
+++ docs/runtime.4863bbca4e25c651c861.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "dfc23a99f878bd49718b";
+/******/ 	var hotCurrentHash = "4863bbca4e25c651c861";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
