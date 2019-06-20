d054609f45c114db198343bbd1daf45e43adc83b
Brais <braisva@gmail.com>
2019-06-20 15:54:37 +0200

    Polygons worked just fine

--- src/views/index.js
+++ src/views/index.js
@@ -1,8 +1,43 @@
 import "../styles/pageStyles.scss";
 
+let ratings = document.getElementsByClassName('c-rating');
+
+for (let item of ratings) {
+    console.log(item);
+    item.addEventListener('click', (e) => {
+        console.log(e.currentTarget.classList.contains('c-rating') ,e);
+        if(!e.target.classList.contains('c-rating')){
+            let rootElement = e.target.farthestViewportElement.parentElement;
+            rootElement.classList.remove('empty');
+            console.log(rootElement.getElementsByClassName('active'));
+            for(let elem of rootElement.getElementsByClassName('active'))elem.classList.remove('active');
+            e.target.farthestViewportElement.classList.add('active');
+        }
+    })
+}
+
+//let pie = new BridePie();
 
 
-/*
+
+function filterNumbersFromArray(data) {
+  // This could be made with arr.filter but it returns a new array that needs to be reasigned to the original var,
+  //(therby, filter doesnt change the original array and its not applicable to this exercise)
+  let i = data.length
+  while(i-- != 0) {
+    if(typeof data[i] != 'number' ) {
+        data.splice(i,1);
+    }
+  }
+  
+}
+
+var arr = [1, 'a', 'b', 2];
+filterNumbersFromArray(arr);
+for (var i = 0; i < arr.length; i++)
+  console.log(arr[i]);
+
+
 const getPolygon = function (countOfSegments) {
   // Create a Polygon Object
 
@@ -57,42 +92,3 @@ generic.whatIs(); // Open a new window that links to https://en.wikipedia.org/wi
 
 
 const emptyPolygon = getPolygon(); // Emit a log message: 'A polygon needs to have at least 3 segments, it will be set to 3' and set it to 3
-
-*/
-
-let ratings = document.getElementsByClassName('c-rating');
-
-for (let item of ratings) {
-    console.log(item);
-    item.addEventListener('click', (e) => {
-        console.log(e.currentTarget.classList.contains('c-rating') ,e);
-        if(!e.target.classList.contains('c-rating')){
-            let rootElement = e.target.farthestViewportElement.parentElement;
-            rootElement.classList.remove('empty');
-            console.log(rootElement.getElementsByClassName('active'));
-            for(let elem of rootElement.getElementsByClassName('active'))elem.classList.remove('active');
-            e.target.farthestViewportElement.classList.add('active');
-        }
-    })
-}
-
-//let pie = new BridePie();
-
-
-
-function filterNumbersFromArray(data) {
-  // This could be made with arr.filter but it returns a new array that needs to be reasigned to the original var,
-  //(therby, filter doesnt change the original array and its not applicable to this exercise)
-  let i = data.length
-  while(i-- != 0) {
-    if(typeof data[i] != 'number' ) {
-        data.splice(i,1);
-    }
-  }
-  
-}
-
-var arr = [1, 'a', 'b', 2];
-filterNumbersFromArray(arr);
-for (var i = 0; i < arr.length; i++)
-  console.log(arr[i]);
