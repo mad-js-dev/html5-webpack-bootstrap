66bcb047e014ee5d025abd783cb768e9a12acef5
Brais <braisva@gmail.com>
2019-06-25 23:19:59 +0200

    base icons spacing

--- docs/runtime.e1b4cae72985f5c7d002.js
+++ docs/runtime.e1b4cae72985f5c7d002.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "92a18b9eb985ccdd0455";
+/******/ 	var hotCurrentHash = "e1b4cae72985f5c7d002";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
